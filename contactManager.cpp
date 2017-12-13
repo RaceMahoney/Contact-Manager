ab6cc23322db178c29849020ad088502187b1e69
Race <race.mahoney@gmail.com>
2017-12-13 11:04:53 -0500

    added listing feature - begun work on editing feature - known bugs

--- contactManager.cpp
+++ contactManager.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
- #include <unistd.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 void add(fstream& file){
+
   cout << "                   Contact Format" << endl;
   cout << "==========================================================" << endl;
   cout << endl;
@@ -15,11 +19,11 @@ void add(fstream& file){
   string name, phone, address, email;
   cout << "\nEnter name: ";
   getline(cin, name);
-  cout << "Enter Phone Number:";
+  cout << "Enter Phone Number: ";
   getline(cin, phone);
-  cout << "Enter Address:";
+  cout << "Enter Address: ";
   getline(cin, address);
-  cout << "Enter Email:";
+  cout << "Enter Email: ";
   getline(cin, email);
 
   if(file.is_open()){
@@ -30,7 +34,7 @@ void add(fstream& file){
     cout << "==========================================================" << endl;
     file << name << "," << phone << "," << address << "," << email << endl;
   }else {
-   cout << "Could write to file: " << endl;
+   cout << "Could not write to file: " << endl;
   }
 
   sleep(2);
@@ -38,20 +42,85 @@ void add(fstream& file){
 
 
 void list(fstream& file){
+
+  cout << "                   Contact List" << endl;
+  cout << "==========================================================" << endl;
+
+  if(file.is_open()){
+    string line;
+    int lineNumber = 1;
+
+    while ( getline (file,line) )
+    {
+      cout << lineNumber << ") " << line << '\n';
+      lineNumber++;
+    }
+  } else {
+    cout << "File is not open" << endl;
+  }
+  cout << "==========================================================" << endl;
+
+}
+
+void edit(fstream& file){
+
+  cout << "                   Edit Contact" << endl;
+  cout << "==========================================================" << endl;
+  cout << "           Select Contact Entry to Edit\n" << endl;
+  list(file);
+
+  //put file info into vector
+  vector<string> info;
   if(file.is_open()){
 
     string line;
+    int index = 0;
 
-    while(!file.eof()){ //end of file
-    getline(file, line);
-    cout << line << endl;
+    while ( getline (file,line) )
+    {
+      info.push_back(line);
+      index++;
+    }
+  } else {
+    cout << "File is not open" << endl;
   }
-}
+
+  //determine which info to modify
+  int input;
+  cout << "\n > ";
+  cin >> input;
+
+  string name, phone, address, email;
+  cout << "\nEnter Name: ";
+  cin >> name;
+  cout << "Enter Phone Number:";
+  cin >> phone;
+  cout << "Enter Address:";
+  cin >> address;
+  cout << "Enter Email:";
+  cin >> email;
+
+  //save info to single string and add to vector
+   string allInfo = name + "," + phone + "," + address + "," + email;
+   info.at(input) = allInfo;
+
+  // //create new file
+  //TODO fix out of range exception
+  fstream temp;
+  temp.open("temp.txt");
+  if(temp.is_open()){
+    for(unsigned int i = 0; i<info.size();i++){
+      temp << info[i] << endl;
+    }
+  }
+
+
+
 }
 
 int main(){
   fstream file;
-  file.open("contacts.txt", ios::out);
+
 
   string answer = "";
   cout << "**** Welcome to the Contact Managment System ****" << endl;
@@ -59,29 +128,41 @@ while(answer != "5"){
 
   cout << endl;
   cout << "                     MAIN MENU                 " << endl;
-  cout << "            ===========================        " << endl;
+  cout << "           =============================       " << endl;
   cout << "            [1] Add a new Contact" << endl;
   cout << "            [2] List all Contacts" << endl;
   cout << "            [3] Edit a Contact" << endl;
   cout << "            [4] Delete a Contact" << endl;
   cout << "            [5] Exit" << endl;
-  cout << "            ===========================" << endl;
+  cout << "           =============================" << endl;
 
+  cout << "\n > ";
   getline(cin, answer);
 
   if(answer == "1"){
+        file.open("contacts.txt", fstream::app);
         add(file);
-    } else if (answer == "2") {
-        list(file);
-    } else if(answer == "3"){
-      //edit()
-    } else if(answer == "4") {
-      //delete()
-    } else if (answer == "5") {
-      cout << "Thank you foe using the Contact Managment System!" << endl;
-    } else {
-      cout << "Enter valid menu option" << endl;
+        file.close();
     }
+  if (answer == "2") {
+        file.open("contacts.txt");
+        list(file);
+        file.close();
+    }
+  if(answer == "3"){
+    file.open("contacts.txt");
+    edit(file);
+    file.close();
+    }
+  if(answer == "4") {
+      //delete()
+    }
+  if (answer == "5") {
+      cout << "Thank you for using the Contact Managment System!" << endl;
+    }
+    // } else {
+    //   cout << "Enter valid menu option" << endl;
+    // }
 }
 
 
